1c229d509f17c90967c1cee8854ef6c3683fdf50
Julian <julikq22@gmail.com>
2023-11-16 18:59:37 +0100

    Adding checkout section tests

--- cypress/e2e/tests/PDP.cy.js
+++ cypress/e2e/tests/PDP.cy.js
@@ -23,17 +23,12 @@ describe('Validate adding products to shopping carts correctly', () => {
         cy.contains('Tienda para perros: accesorios y comida ').click()
         cy.url().should('contain','/shop/tienda_perros')
     })
-    it.only('Verify pop up is displayed after user adds a product to shopping cart', () => {
+    it('Verify pop up is displayed after user adds a product to shopping cart', () => {
         cy.visit('/shop/tienda_perros/pienso_perros')
         cy.get('[data-zta="quantityStepperIncrementButton"]').first().click()
         cy.get('[data-zta="quantityStepperInput"]').first().should('have.value','1')
         cy.get('[data-zta="add-to-cart"]').first().click()
         cy.get('[data-testid="popoverWrapper"]').as('popUp').should('be.visible')
         cy.get('[data-zta="cart-feedback-icon"]').should('be.visible')
-        cy.request('https://www.zooplus.es/checkout/api/cart-api/v1/cart/42b98bf3-c401-4dec-bc0f-c94e68e956e7/set-article-quantity-command').as('shoppingCart')
-        cy.get('@shoppingCart').should((response)=>{
-            expect(response.body).to.have.length(0)
-            expect(response.body).to.have.property('quantity')
-        });
     });
 });
---
+++ cypress/e2e/tests/checkout.cy.js
@@ -0,0 +1,47 @@
+import '@testing-library/cypress/add-commands'
+import { removeLogs } from '../../support/helper/removeLogs';
+removeLogs();
+const acceptCookies = () =>{cy.get('#onetrust-accept-btn-handler').click()}
+
+describe('Validate shopping cart', () => {
+    beforeEach('Precondition: Add a product to the shopping cart',()=>{
+        cy.visit('/shop/tienda_perros/pienso_perros')
+        acceptCookies();
+        cy.get('[data-zta="page-title"]').should('contain','Pienso para perros')
+        cy.get('[data-zta="quantityStepperIncrementButton"]').first().click()
+        cy.get('[data-zta="quantityStepperInput"]').first().should('have.value','1')
+        cy.get('[data-zta="add-to-cart"]').first().click()
+        cy.get('[data-testid="MiniCartItemsCount"]').should('contain','1')
+        })        
+    it('Validate user deletes the product correctly', () => {
+        cy.visit('/checkout/cart')
+        cy.get('[data-zta="H1UIC"]').should('contain','Mi cesta')
+        cy.get('[data-zta="quantityStepperDecrementButton"]').click()
+        cy.get('[data-zta="removedArticleMsg"]').should('contain.text','El artículo se ha eliminado correctamente.')
+        cy.get('[data-zta="H1UIC"]').should('contain','Tu cesta está vacía')
+    })
+    it('Validate user can undo when deletes a product', () => {
+        cy.visit('/checkout/cart')
+        cy.get('[data-zta="H1UIC"]').should('contain','Mi cesta')
+        cy.get('[data-zta="quantityStepperDecrementButton"]').click()
+        cy.get('[data-zta="removedArticleMsg"]').should('contain.text','El artículo se ha eliminado correctamente.')
+        cy.get('[data-zta="H1UIC"]').should('contain','Tu cesta está vacía')
+        cy.contains('Deshacer').click()
+        cy.get('[data-zta="H1UIC"]').should('contain','Mi cesta')
+    })
+    it.only('Validate user cannot proceed to checkout when the price is under 15 euros', () => {
+        cy.visit('/checkout/cart')
+        cy.get('[data-zta="overviewSubTotalValue"]').invoke('text').then(($price) =>{
+            const price = parseInt($price);
+            const checkoutBtn = cy.get('[data-zta="gotoPreviewBottom"]');
+
+            if (price < 15) {
+                checkoutBtn.should('be.disabled');
+                cy.get('[data-zta="alertText"]').should('contain.text','Recuerda: el importe mínimo de pedido es de 15,00 € (más gastos de envío)')
+            }
+            else {
+                checkoutBtn.should('be.enabled');
+            }
+        })  
+    })
+})
